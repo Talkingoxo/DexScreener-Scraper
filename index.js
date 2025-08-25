806c9c94219db8fa8567db76f5cb1186794d1890
Talkingoxo <116345918+Talkingoxo@users.noreply.github.com>
2025-08-25 20:28:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-app.post('/', async (req, res) => {
+app.post('/', (req, res) => {
   const { url } = req.body;
   res.status(200).send('ok');
   
   if (url) {
-    const requests = Array(76).fill().map(() => 
-      fetch(url, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({}),
-        timeout: 30000
-      }).catch(() => {})
-    );
-    
-    Promise.all(requests);
+    for (let i = 0; i < 76; i++) {
+      setImmediate(() => {
+        fetch(url, {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({}),
+          timeout: 10000
+        }).catch(() => {});
+      });
+    }
   }
 });
 
