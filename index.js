1dfb80c93917bcc233a7cc2ae01d91730788e11b
Talkingoxo <116345918+Talkingoxo@users.noreply.github.com>
2025-08-25 04:28:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const https = require('https');
-app.post('/', async (req, res) => {
+app.post('/', (req, res) => {
 const { url } = req.body;
 res.status(200).send('ok');
 if (url) {
-const promises = [];
 for (let i = 0; i < 76; i++) {
-promises.push((async () => {
 const agent = new https.Agent({ keepAlive: false });
-try {
-const response = await fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json', 'Connection': 'close' }, body: JSON.stringify({}), timeout: 8000, agent });
-await response.text();
-agent.destroy();
-} catch (error) {
-agent.destroy();
+fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json', 'Connection': 'close' }, body: JSON.stringify({}), timeout: 8000, agent }).then(response => response.text()).then(() => agent.destroy()).catch(() => agent.destroy());
 }
-})());
-}
-await Promise.all(promises);
 }
 });
 app.get('/', (req, res) => {
