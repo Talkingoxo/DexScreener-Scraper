3791b1364e92d8a5cab6458a7ab744768f9b39d1
Talkingoxo <116345918+Talkingoxo@users.noreply.github.com>
2025-09-16 00:20:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/', (req, res) => {
   const lastPart = url.slice(slashIndex + 1);
   const count = +lastPart || 1;
   const targetUrl = encodeURIComponent(url.slice(0, slashIndex + 1));
-  const apiKey = '2a33de63f0565655000000fakef1cb33f5857a3fe00c5';
+  const apiKey = '2a33de63f0054f1cb33f5857a3fe00c5';
   for (let i = 0; i < count; i++) {
     const country = countries[i % 24];
     const apiUrl = `https://api.scrapingant.com/v2/general?x-api-key=${apiKey}&url=${targetUrl}&proxy_country=${country}&proxy_type=datacenter&browser=false`;
