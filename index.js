1a839864c6487a2d5b28ade6f484b3f4937afd35
Talkingoxo <116345918+Talkingoxo@users.noreply.github.com>
2025-09-17 09:23:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 10000;
+app.use(express.json());
 app.use((req, res, next) => {console.log(`Hey i am Log - ${req.method} ${req.url} from ${req.get('User-Agent')}`);next();});
 app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/trigger', (req, res) => res.send('triggered'));
 app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
