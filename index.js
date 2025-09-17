3f18fc9bc72b87de1379253678f0e22531584be4
Talkingoxo <116345918+Talkingoxo@users.noreply.github.com>
2025-09-17 09:09:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 10000;
 console.log("Hey i am Log");
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
